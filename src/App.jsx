88ea7c0e0d0ad33d8f6ec3ff18e4fc405029591d
john0 <jcampusano@k3d.do>
2023-07-26 21:03:48 -0400

    refactorizing app

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,50 @@
-import './App.css';
 import React from 'react';
-import Inventory from './pages/Inventory';
+import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom';
 import Home from './pages/Home';
+import Inventory from './pages/Inventory';
 import NotFound from './pages/NotFound';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import ProductCategories from './pages/ProductCategories';
 import CreateProductCategory from './pages/CreateProductCategory';
 import ProtectedRoutes from './utils/ProtectedRoutes';
 import AddProduct from './pages/AddProduct';
+
 function App() {
+  // handles user authentication state
+  const isAuthenticated = false; 
+
   return (
     <div className="App">
-      <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Login />} />
-            <Route element={<ProtectedRoutes/>}>
-                <Route path="/dashboard" element={<Home/>}/>
-                <Route path="/inventory" element={<Inventory/>}/>
-                <Route path="/productcategories" element={<ProductCategories/>}/>
-                <Route path="/createproductcategory" element={<CreateProductCategory/>}/>
-                <Route path="/addProduct" element={<AddProduct/>}/>
-                <Route path="*" element={<NotFound/>}/>
-              </Route>
-          </Routes>
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          /* conditional redirection */
+          <Route path="/" element={isAuthenticated ? <Home /> : <Redirect to="/login" />} />
+          <Route path="/login" element={<Login />} />
+
+          /* Rutas protegidas que requieren autenticación */
+          <Route
+            element={
+              isAuthenticated ? (
+                <ProtectedRoutes>
+                  <Route path="/dashboard" element={<Home />} />
+                  <Route path="/inventory" element={<Inventory />} />
+                  <Route path="/productcategories" element={<ProductCategories />} />
+                  <Route path="/createproductcategory" element={<CreateProductCategory />} />
+                  <Route path="/addProduct" element={<AddProduct />} />
+                </ProtectedRoutes>
+              ) : (
+                <Redirect to="/login" />
+              )
+            }
+          />
+
+          /* Ruta para manejar cualquier otra URL no encontrada */
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
+
 export default App;
+
