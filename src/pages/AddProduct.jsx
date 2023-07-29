ea26e87ec8a8b3f898d490da1164f2a36b52d723
john0 <jcampusano@k3d.do>
2023-07-28 22:48:45 -0400

    fix login issue

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Menu from '../components/Menu';
+import { Paper, Grid, FormControl, Typography } from '@mui/material';
 function AddProduct() {
     
   return ( 
-    <div className="App">
+    <div className="container">
         <Menu/>
         <h2>Agregar nuevo producto</h2>
+        <Grid>
+          <Paper>
+              <FormControl>
+                <Typography variant="h2" align="center">
+                  Add product
+                </Typography>
+              </FormControl>
+          </Paper>
+        </Grid>
     </div>
   )
 }
